ff72f5d075ad941d7f32cfef5f4275fcfd70f06b
Liz Abinante <eabinante@gmail.com>
2014-02-17 14:10:32 +0000

    renamed MenuItems to Dish

--- dish.rb
+++ dish.rb
@@ -1,6 +1,6 @@
 require_relative 'loader'
 
-class MenuItem
+class Dish
   attr_reader :item, :price
 
   def initialize(item, price)
--- menu.rb
+++ menu.rb
@@ -1,13 +1,13 @@
 require_relative 'loader'
 
 class Menu
-  attr_reader :items
+  attr_reader :dishes
 
   def initialize(opts_hash)
-    @items = []
+    @dishes = []
 
     opts_hash.each do |item, price|
-      @items << MenuItem.new(item, price)
+      @dishes << MenuItem.new(item, price)
     end
   end
 end
